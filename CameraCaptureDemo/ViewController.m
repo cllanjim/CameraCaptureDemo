346d4fb9a43eb1e03c1781b43d02545507b6373a
bzhou <bzhou@grandstream.cn>
2016-03-24 16:02:25 +0800

    1)support openGLES(local camera data display); 2)set frame rate;

--- CameraCaptureDemo/ViewController.m
+++ CameraCaptureDemo/ViewController.m
@@ -8,6 +8,16 @@
 
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/gltypes.h>
+#import <GLKit/GLKView.h>
+#import <CoreImage/CoreImage.h>
+
+typedef NS_ENUM(NSInteger, VideoDisplayMode)
+{
+    VideoDisplayMode_PreviewLayer = 0,
+    VideoDisplayMode_OpenGLES = 1
+};
 
 @interface ViewController ()
 <AVCaptureVideoDataOutputSampleBufferDelegate>
@@ -17,9 +27,18 @@
     AVCaptureDeviceInput *captureInputFront;
     AVCaptureDeviceInput *captureInputBack;
     AVCaptureVideoDataOutput *captureOutput;
+    AVCaptureDevice *currentCaptureDevice;
     dispatch_queue_t captureQueue;
     AVCaptureVideoPreviewLayer *previewLayer;
     AVCaptureDevicePosition currentCameraPosition;
+    
+    AVCaptureConnection *captureConnection;
+    EAGLContext *openGLContext;
+    GLKView *glkView;
+    CIContext *ciContext;
+    
+    VideoDisplayMode currentDisplayMode;
+    NSUInteger frames;
 }
 
 @end
@@ -28,16 +47,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    [self initCaptureSession];
-    [self initPreviewLayer];
+
+    currentDisplayMode = VideoDisplayMode_OpenGLES;
+    frames = 10;
     
-    UIView *view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    view.backgroundColor = [UIColor grayColor];
-    previewLayer.frame = view.frame;
-    [view.layer addSublayer:previewLayer];
-//    [view.layer insertSublayer:previewLayer atIndex:0];
-    [self.view addSubview:view];
+    [self initCaptureSession];
+    switch (currentDisplayMode) {
+        case VideoDisplayMode_PreviewLayer:
+            [self initPreviewLayer];
+            break;
+        case VideoDisplayMode_OpenGLES:
+            [self initOpenGLES];
+            break;
+        default:
+            break;
+    }
     
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
     btn.frame = CGRectMake(0, 0, 100, 44);
@@ -54,6 +78,14 @@
     [btn setTitle:@"切换摄像头" forState:UIControlStateNormal];
     [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
     [self.view addSubview:btn];
+    
+    btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    btn.frame = CGRectMake(0, 0, 100, 44);
+    btn.center = CGPointMake(self.view.center.x, self.view.center.y + 88);
+    [btn addTarget:self action:@selector(stopCapture) forControlEvents:UIControlEventTouchUpInside];
+    [btn setTitle:@"停止采集" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [self.view addSubview:btn];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -63,7 +95,13 @@
 
 - (void)startCapture
 {
+//    NSLog(@"%@", currentCaptureDevice.activeFormat);
     [captureSession startRunning];
+}
+
+- (void)stopCapture
+{
+    [captureSession stopRunning];
 }
 
 - (void)changeCamera
@@ -77,6 +115,36 @@
     [self changeCameraPosition:currentCameraPosition];
 }
 
+/*
+ call this api after following Configuration
+ *
+ *
+ On iOS, the receiver's activeVideoMaxFrameDuration resets to its default value under the following conditions:
+ - The receiver's activeFormat changes
+ - The receiver's AVCaptureDeviceInput's session's sessionPreset changes
+ - The receiver's AVCaptureDeviceInput is added to a session*/
+- (BOOL)setFramerateDevice:(AVCaptureDevice *)device Rate:(CMTime)frameDuration
+{
+    NSError *error;
+    NSArray *supportedFrameRateRanges = [device.activeFormat videoSupportedFrameRateRanges];
+    BOOL frameRateSupported = NO;
+    for (AVFrameRateRange *range in supportedFrameRateRanges) {
+        if (CMTIME_COMPARE_INLINE(frameDuration, >=, range.minFrameDuration) &&
+            CMTIME_COMPARE_INLINE(frameDuration, <=, range.maxFrameDuration)) {
+            frameRateSupported = YES;
+        }
+    }
+    
+    if (frameRateSupported && [device lockForConfiguration:&error]) {
+        [device setActiveVideoMaxFrameDuration:frameDuration];
+        [device setActiveVideoMinFrameDuration:frameDuration];
+        [device unlockForConfiguration];
+        return YES;
+    }
+    NSLog(@"frame rate set error:%@!", error);
+    return NO;
+}
+
 - (void)initCaptureSession
 {
     captureSession = [[AVCaptureSession alloc] init];
@@ -86,14 +154,32 @@
     for (AVCaptureDevice *device in devices) {
         switch ([device position]) {
             case AVCaptureDevicePositionBack:
+            {
                 captureInputBack = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+            }
                 break;
             case AVCaptureDevicePositionFront:
+            {
                 captureInputFront = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+                currentCaptureDevice = captureInputFront.device;
+            }
                 break;
             default:
                 break;
         }
+    }
+    
+//    captureConnection = [[AVCaptureConnection alloc]init];
+//    if ([captureSession canAddConnection:captureConnection]) {
+//        [captureSession addConnection:captureConnection];
+//    }
+//    captureConnection.videoOrientation = AVCaptureVideoOrientationLandscapeLeft;
+    
+    if ([captureSession canSetSessionPreset:AVCaptureSessionPreset1280x720]) {
+        captureSession.sessionPreset = AVCaptureSessionPreset1280x720;
+    }
+    else {
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto;
     }
     currentCameraPosition = AVCaptureDevicePositionFront;
     [self changeCameraPosition:currentCameraPosition];
@@ -121,13 +207,27 @@
         return NO;
     }
     previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:captureSession];
-    previewLayer.backgroundColor = UIColor.blackColor.CGColor;
+    previewLayer.backgroundColor = [UIColor clearColor].CGColor;
     previewLayer.videoGravity = AVLayerVideoGravityResizeAspect;
     if (previewLayer.connection.isVideoOrientationSupported) {
 //        previewLayer.connection.videoOrientation = AVCaptureVideoOrientationLandscapeLeft;
     }
     
+    UIView *view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    view.backgroundColor = [UIColor grayColor];
+    previewLayer.frame = view.frame;
+    [view.layer addSublayer:previewLayer];
+    [self.view addSubview:view];
+    
     return YES;
+}
+
+- (void)initOpenGLES
+{
+    openGLContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    glkView = [[GLKView alloc] initWithFrame:[UIScreen mainScreen].bounds context:openGLContext];
+    ciContext = [CIContext contextWithEAGLContext:openGLContext];
+    [self.view addSubview:glkView];
 }
 
 - (void)changeCameraPosition:(AVCaptureDevicePosition)position
@@ -146,6 +246,18 @@
         }
     }
     [captureSession commitConfiguration];
+    
+    switch (position) {
+        case AVCaptureDevicePositionFront:
+            [self setFramerateDevice:captureInputFront.device Rate:CMTimeMake(1, (int32_t)frames)];
+            break;
+        case AVCaptureDevicePositionBack:
+            [self setFramerateDevice:captureInputBack.device Rate:CMTimeMake(1, (int32_t)frames)];
+            break;
+        default:
+            break;
+    }
+    
 }
 
 - (void) captureOutput:(AVCaptureOutput *) captureOutput
@@ -153,6 +265,25 @@
         fromConnection:(AVCaptureConnection *)connection
 {
     NSLog(@"%s", __FUNCTION__);
+    if (VideoDisplayMode_OpenGLES != currentDisplayMode) {
+        return;
+    }
+//    AVCaptureMetadataOutput
+    CFDictionaryRef dic = CMCopyDictionaryOfAttachments(nil, sampleBuffer, kCMAttachmentMode_ShouldPropagate);
+    
+//    (nil, imageDataSampleBuffer, CMAttachmentMode(kCMAttachmentMode_ShouldPropagate)).takeUnretainedValue()
+    
+    CVImageBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CIImage *image = [CIImage imageWithCVPixelBuffer:pixelBuffer];
+    if ([EAGLContext currentContext] != openGLContext) {
+        [EAGLContext setCurrentContext:openGLContext];
+    }
+//    NSLog(@"x:%f y:%f w:%f h:%f", image.extent.origin.x, image.extent.origin.y, image.extent.size.width, image.extent.size.height);
+    [glkView bindDrawable];
+    [ciContext drawImage:image inRect:image.extent fromRect:image.extent];
+    [glkView display];
+    
+    
 }
 
 @end
